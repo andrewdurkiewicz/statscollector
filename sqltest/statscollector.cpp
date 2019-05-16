6afdf7059890d5a095dc4bb5b15a70818eb7f6db
user <andrewdurkiewicz@gmail.com>
2019-05-16 13:58:14 -0400

    was able to get the sqlite3 db working. Venkat told me that we need to use csv over databases. looking at making the change now

--- sqltest/statscollector.cpp
+++ sqltest/statscollector.cpp
@@ -2,7 +2,7 @@
 #include <sqlite3.h> 
 #include <string> 
 #include <stdio.h>
-  
+#include <unistd.h>      
 using namespace std; 
   
 static int callback(void* data, int argc, char** argv, char** azColName) 
@@ -24,24 +24,26 @@ void SQL_CMD(string comand);
 sqlite3 *db;
 char *zErrMsg = 0;
 int rc = sqlite3_open("testDB.db", &db);
+int i = 0;
 int main(int argc, char* argv[]){
-   SQL_CMD("ATTACH DATABASE 'StatsCollector' as 'statscollector';");
+   while(1){
+      i++;
+      SQL_CMD("ATTACH DATABASE 'StatsCollector' as 'statscollector';");
+      
+      cout << "STATE OF TABLE BEFORE INSERT" << endl;
+      SQL_CMD("SELECT * FROM stats WHERE 1;");
 
-   cout << "STATE OF TABLE BEFORE INSERT" << endl;
-
-   SQL_CMD("SELECT * FROM stats WHERE 1;");
-
-   for(int i = 5; i < 60; i++){
       char buffer[200];
       sprintf(buffer,"INSERT INTO stats (Time,UL,DL,Throughput) VALUES (%i,%-.2f,%-.2f,%-.2f);",i, i*2.2, i*4.3, i*1.5);
       SQL_CMD(buffer);
 
+      cout << "STATE OF TABLE AFTER INSERT" << endl;
+      SQL_CMD("SELECT * FROM stats WHERE 1;");
+      usleep(10000);
 
    }
-   cout << "STATE OF TABLE AFTER INSERT" << endl;
-
-   SQL_CMD("SELECT * FROM stats WHERE 1;");
    sqlite3_close(db);
+
 }
 
 
