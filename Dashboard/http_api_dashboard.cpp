dd62bfa1b8683d1ed1c335ff85a1614416c5d19d
user <andrewdurkiewicz@gmail.com>
2019-05-22 17:43:51 -0400

    uploading stats_out.json, which will be used for georges files

--- Dashboard/http_api_dashboard.cpp
+++ Dashboard/http_api_dashboard.cpp
@@ -1,30 +1,31 @@
 #include <http_api_dashboard.h>
-
+#include <fstream>
 #include <json.hpp>
 #include <sqlite3.h>
 #include <SQLiteCpp/SQLiteCpp.h>
 #include <iostream>
-
+#include <iomanip>
 using nlohmann::json;
 using namespace std;
-
+//export LD_LIBRARY_PATH=.
+// g++ -std=c++11 http_api_dashboard.cpp -I. libSQLiteCpp.so libsqlite3.so -lpthread -ldl -o http_api_executable && ./http_api_executable 
 void getgraph() {
     try{
         SQLite:: Database db("StatsCollector.db", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
         string q = "select Time, UL, DL, Throughput from stats where 1";
         
         SQLite::Statement query(db,q);
-        json j = json::array();
+        json file = json::array();
         while(query.executeStep()) {
             json row;
-            row["time"] = (int) query.getColumn(0);
-            row["time2"] = (const char *) query.getColumn(1);
-            row["time3"] = (const char *) query.getColumn(2);
-            row["time4"] = (const char *) query.getColumn(3);
-            j.push_back(row);
+            row["Time"] = (int) query.getColumn(0);
+            row["UL"] = (const char *) query.getColumn(1);
+            row["DL"] = (const char *) query.getColumn(2);
+            row["Throughput"] = (const char *) query.getColumn(3);
+            file.push_back(row);
         }
-
-        cout << j << endl;
+        ofstream o("stats_out.json");
+		o << setw(4) << file << endl;
     }
     catch (const SQLite::Exception & ex) {
         cout << "ERROR:" << ex.what();
