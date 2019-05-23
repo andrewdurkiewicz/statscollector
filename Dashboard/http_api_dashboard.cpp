87ca321b392111ae7ffcec3b421dfc5196295866
user <andrewdurkiewicz@gmail.com>
2019-05-23 17:01:54 -0400

    was able to generate the so file for jsoncpp

--- Dashboard/http_api_dashboard.cpp
+++ Dashboard/http_api_dashboard.cpp
@@ -1,21 +1,26 @@
 #include <http_api_dashboard.h>
 #include <fstream>
-#include <json.hpp>
+#include <json/json.h>
 #include <sqlite3.h>
 #include <SQLiteCpp/SQLiteCpp.h>
 #include <iostream>
+#include <Response.h>
+#include <Request.h>
 #include <iomanip>
-using nlohmann::json;
+
 using namespace std;
 //export LD_LIBRARY_PATH=.
 // g++ -std=c++11 http_api_dashboard.cpp -I. libSQLiteCpp.so libsqlite3.so -lpthread -ldl -o http_api_executable && ./http_api_executable 
-void getgraph() {
+void getgraph(Request & req, Response & res) {
     try{
         SQLite:: Database db("StatsCollector.db", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
         string q = "select Time, UL, DL, Throughput from stats where 1";
         
         SQLite::Statement query(db,q);
         json file = json::array();
+
+        Json::Value response;
+
         while(query.executeStep()) {
             json row;
             row["Time"] = (int) query.getColumn(0);
@@ -25,7 +30,7 @@ void getgraph() {
             file.push_back(row);
         }
         ofstream o("stats_out.json");
-		o << setw(4) << file << endl;
+		o << file << endl;
     }
     catch (const SQLite::Exception & ex) {
         cout << "ERROR:" << ex.what();
@@ -35,4 +40,8 @@ void getgraph() {
 int main() {
     getgraph();
     return 0;
+}
+
+void initDashboardRoutes(Express::Router & router){
+    router.get("/gendata",getgraph);
 }
