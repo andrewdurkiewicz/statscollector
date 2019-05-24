325afd8e1d065a06a9c563f56bac745e0dd25d2f
user <andrewdurkiewicz@gmail.com>
2019-05-24 14:24:37 -0400

    checkpoint before beginning routing

--- Dashboard/http_api_dashboard.cpp
+++ Dashboard/http_api_dashboard.cpp
@@ -11,34 +11,62 @@ using namespace std;
 //To Run:
 //  g++ http_api_dashboard.cpp -I. libSQLiteCpp.so libsqlite3.so -lpthread -ldl libsqlite3.so libSQLiteCpp.so  -ljsoncpp /home/user/jsoncpp/build-shared/libjsoncpp.so -o http_api_executable && ./http_api_executable 
 
-void getgraph() {
-    try{
-        SQLite:: Database db("StatsCollector.db", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
-        string q = "select Time, UL, DL, Throughput from stats where 1";
-        
-        SQLite::Statement query(db,q);
 
-        Json::Value response;
-
-        while(query.executeStep()) {
-    		Json::Value row;
-            row["Time"] = (int) query.getColumn(0);
-            row["UL"] = (const char *) query.getColumn(1);
-            row["DL"] = (const char *) query.getColumn(2);
-            row["Throughput"] = (const char *) query.getColumn(3);
-            response.append(row);
-        }
-        ofstream o("stats_out.json");
-        o << response << endl;
+Json::Value response;
+SQLite:: Database db("StatsCollector.db", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+string q = "select * from stats where Time";    
+void getUL(), getDL(), getThroughput();
 
 
-    }
-    catch (const SQLite::Exception & ex) {
-        cout << "ERROR:" << ex.what();
-    }
-}
+
 
 int main() {
-    getgraph();
+    getUL();
+    getDL();
+
+    getThroughput();
     return 0;
+}
+
+
+void getUL(){
+    SQLite::Statement query(db,q);
+
+    while(query.executeStep()){
+        Json::Value row;
+        row["Time"] = (int) query.getColumn("Time");
+        row["UL"] = (const char *) query.getColumn("UL");
+        response.append(row);
+    }
+        ofstream o("UL.json");
+        o << response << endl;
+        cout << response;
+}
+
+void getDL(){
+        SQLite::Statement query(db,q);
+
+    while(query.executeStep()){
+        Json::Value row;
+        row["Time"] = (int) query.getColumn("Time");
+        row["DL"] = (const char *) query.getColumn("DL");
+        response.append(row);
+    }
+        ofstream o("DL.json");
+        o << response << endl;
+        cout << response;
+}
+
+void getThroughput(){
+        SQLite::Statement query(db,q);
+
+    while(query.executeStep()){
+        Json::Value row;
+        row["Time"] = (int) query.getColumn("Time");
+        row["Throughput"] = (const char *) query.getColumn("DL");
+        response.append(row);
+    }
+        ofstream o("Throughput.json");
+        o << response << endl;
+        cout << response;
 }
